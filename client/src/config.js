a06d95be09874d96a8db77806371aaad0677b921
Martin Stenflo <stenflo@gmail.com>
2021-01-27 12:21:07 -0800

    handle aws upload

---
+++ client/src/config.js
@@ -0,0 +1,8 @@
+const S3Config = {
+  "S3Bucket": "tribly",
+  "S3Region": "us-west-1",
+  "S3AccessKeyID": "AKIAJ37AB6ARAFYWRK6Q",
+  "S3SecretAccessKey": "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"
+}
+
+export default S3Config;
